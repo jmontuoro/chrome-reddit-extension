4a968836efb29164b2a95c1f9f98d3f1ce653c27
Jackson Montuoro <55864869+jmontuoro@users.noreply.github.com>
2025-07-07 16:41:28 -0400

    Update main.py
    enabled CORS, cross origin resource sharing, which is expected when a browser extension tries to fetch from a third-party server like our cloud run backend.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/receive_url', methods=['POST'])
 def receive_url():
     data = request.get_json()
-    print("🔗 Received URL:", data.get('url'))
+    print("Received URL:", data.get('url'))
     return jsonify({"status": "URL received", "received_url": data.get('url')}), 200
 
 @app.route('/')
